3c2cd160152e6f62d98fc76f4b83ccb9c6de5a94
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-11-01 23:36:34 +0300

    Update sms5.js

--- sms5.js
+++ sms5.js
@@ -51,12 +51,47 @@ $( "li[data-tab='tab-31']" ).click();
           document.getElementById("phone1955").click();
     document.getElementById("phone1956").click();
 }
-  if (arr[2].indexOf('Мари')>=1){
-  document.getElementById("phone1950").click();
-    document.getElementById("phone1951").click();
-          document.getElementById("phone1952").click();
-          document.getElementById("phone1955").click();
-    document.getElementById("phone1956").click();
+  if (arr[2].indexOf('Юрин')>=1){
+  document.getElementById("phone1944").click();
+    document.getElementById("phone1945").click();
+          document.getElementById("phone1946").click();
+         
 }
+  if (arr[2].indexOf('Волж')>=1){
+  document.getElementById("phone1921").click();
+    document.getElementById("phone1922").click();
+         
+}
+   if (arr[2].indexOf('Звениг')>=1){
+  document.getElementById("phone1965").click();
+    document.getElementById("phone1966").click();
+     document.getElementById("phone1967").click();
+         
+}
+   if (arr[2].indexOf('Новотор')>=1){
+  document.getElementById("phone1936").click();
+    document.getElementById("phone1936").click();
+     document.getElementById("phone1937").click();
+         
+}
+  if (arr[2].indexOf('Паран')>=1){
+  document.getElementById("phone1938").click();
+    document.getElementById("phone1939").click();
+     document.getElementById("phone1940").click();
+         
+}
+   if (arr[2].indexOf('Кужен')>=1){
+  document.getElementById("phone1941").click();
+    document.getElementById("phone1942").click();
+     document.getElementById("phone1943").click();
+         
+}
+   if (arr[2].indexOf('Морк')>=1){
+  document.getElementById("phone1947").click();
+    document.getElementById("phone1948").click();
+     document.getElementById("phone1949").click();
+         
+}
+  
 }
 }, 3000);
