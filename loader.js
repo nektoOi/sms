a436ca4848b94f30e276bfce3b2ac3582b97427d
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-11-01 20:51:13 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,22 +1,27 @@
-mainUrl='https://raw.githubusercontent.com/nektoOi/sms/master/';
+mainUrl='https://raw.githubusercontent.com/nektoOi/fast/master/';
 
 function httpGet(theUrl)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); 
-	xmlHttp.send( null );
-   return xmlHttp.responseText;
+	var xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", "/query", theUrl, false); // async=true
+xmlHttp.onload = function (e) {
+  if xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        console.log(xmlHttp.responseText);
+    }
+};
+xmlHttp.send(null);
+   // var xmlHttp = new XMLHttpRequest();
+   // xmlHttp.open( "GET", theUrl, false ); 
+	//xmlHttp.send( null );
+  // return xmlHttp.responseText;
 }
 
 
-if(('    '+document.location.href).indexOf('http://wiki.kortkeros.com/sms2/')>0){
-	actoinScript=mainUrl+'sms5.js';
-	eval(httpGet(actoinScript));
-	
+if(('    '+document.location.href).indexOf('10.78.16.120:8080/argus/')>1){
+	getUrl='gp2s';
 }
-if((document.location.href).indexOf('http://10.78.16.120:8080/argus/')>0){
-	actoinScript=mainUrl+'sms4.js';
-	eval(httpGet(actoinScript));
+if(('    '+document.location.href).indexOf('http://noc.cbx.ru/z')>1){
+	getUrl='fcrz';
 }
 if(('    '+document.location.href).indexOf('http://nagios.ulrt.net/')>1){
 	getUrl='ulnag';
@@ -25,7 +30,12 @@ if(('    '+document.location.href).indexOf('http://fttb.mts-nn.ru/')>1){
 	getUrl='fttbnn';
 }
 
-//if(getUrl.length>1){
+if(getUrl.length>1){
+	actoinScript=mainUrl+'sms5.js';
+	eval(httpGet(actoinScript));
+}else{
+	alert("Данная страница пока не поддерживается(((");
+}
 	
 	//actoinScript=mainUrl+'sms5.js';
 	//eval(httpGet(actoinScript));
