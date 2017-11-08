15330090d0a744f62c2870f52c043394afc38008
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-11-08 10:17:48 +0300

    Update sms4.js

--- sms4.js
+++ sms4.js
@@ -7,8 +7,8 @@ var y = '';
 arr.forEach(function(element) {
     if(element.match(/\[(\d+\.){3}\d+\]/)) y = element;
 });
-var adres = $( "span[class=' sys-comp-editable-section-output  sys-comp-editable-section-writable']" ).text();
-var timedown = document.getElementById("group_interaction_info_form-tab_view-calculate_due_date_form_out").value;
+var adres = $('#group_interaction_info_form-tab_view-node_rule_building_out-node_rule_building_out').text();
+var timedown = $('#group_interaction_info_form-tab_view-calculate_due_date_form_out').text();
 
 var mess = "Недоступно оборудование " + adres +' '+ y + " Время начала аварии " + timedown;
 //$("#group_interaction_info_form-tab_view-node_rule_building_out-node_rule_building_out").html('<input type="button" name="nubex" value="Nubex" />');
