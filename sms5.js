ddd566f682bc76c99cbb11d41a3782cc360d3436
nektoOi <32074045+nektoOi@users.noreply.github.com>
2017-11-24 08:59:14 +0300

    Update sms5.js

--- sms5.js
+++ sms5.js
@@ -5,9 +5,19 @@ setTimeout(function() {
 var result = $("#text").val();
 //var t = "Морки";
 //$('[data-position *= "'+t+'"]').click();
-
 var arr = result.split(',');
 var nameer = arr[1];
+  if (nameer.indexOf('Ульяновская')>=0){
+$( "li[data-tab='tab-10']" ).click();
+  var result = $("#text").val();
+
+
+var arr = result.split(',');
+var t = arr[2].trim();
+$('[data-keyword *= "'+t+'"]').click();
+}
+  
+
 if (nameer.indexOf('Чувашская')>=0){
 $( "li[data-tab='tab-7']" ).click();
   var result = $("#text").val();
