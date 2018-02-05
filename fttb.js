fdc92c10fbd0aa4ab79c1579d0de5bdac104e6e8
nektoOi <32074045+nektoOi@users.noreply.github.com>
2018-02-05 23:23:33 +0300

    Update fttb.js

--- fttb.js
+++ fttb.js
@@ -1,14 +1,4 @@
-function httpGet(theUrl)
-{
-    
-var xmlHttp = new XMLHttpRequest();
-    
-xmlHttp.open( "GET", theUrl, false ); 
-xmlHttp.send( null );
-   
-return xmlHttp.responseText;
 
-}
 
 
 
