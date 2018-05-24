5148301c6010c72cfb86cfae570413b28749bf4b
nektoOi <32074045+nektoOi@users.noreply.github.com>
2018-05-25 01:17:34 +0300

    Create perenaz.js

---
+++ perenaz.js
@@ -0,0 +1,8 @@
+document.getElementById('gi_header-assignTask').click();
+setTimeout(function() {
+document.getElementById('reassign_dialog_form-select_all_worksites_button-1').click();
+}, 1000);
+setTimeout(function() {
+document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').value = '3ЛТП_ИЖ_УСТП-3';
+document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').focus();
+  }, 2000);
