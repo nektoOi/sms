8d6a901e80c74ee52aa0405f0dfd5fd56a132673
nektoOi <32074045+nektoOi@users.noreply.github.com>
2018-06-22 01:01:17 +0300

    Delete perenaz.js

--- perenaz.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('gi_header-assignTask').click();
-setTimeout(function() {
-document.getElementById('reassign_dialog_form-select_all_worksites_button-1').click();
-}, 1000);
-setTimeout(function() {
-document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').value = '3ЛТП_ИЖ_УСТП-3';
-document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').focus();
-  }, 2000);
