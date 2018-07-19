a9584b8ebbde400b1109ec2399541f600fccddc8
nektoOi <32074045+nektoOi@users.noreply.github.com>
2018-07-20 01:15:42 +0300

    Update perenaz.js

--- perenaz.js
+++ perenaz.js
@@ -3,6 +3,6 @@ setTimeout(function() {
 document.getElementById('reassign_dialog_form-select_all_worksites_button-1').click();
 }, 1000);
 setTimeout(function() {
-document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').value = '3ЛТП_ИЖ_УСТП-3';
-document.getElementById('reassign_dialog_form-j_idt1853-inputWorksiteId_input').focus();
+document.getElementById('reassign_dialog_form-j_idt1869-inputWorksiteId_input').value = '3ЛТП_ИЖ_УСТП-3';
+document.getElementById('reassign_dialog_form-j_idt1869-inputWorksiteId_input').focus();
   }, 2000);
