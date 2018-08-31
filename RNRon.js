55567fc04faaaf3821c6d368fda73104bc7db2ad
nektoOi <32074045+nektoOi@users.noreply.github.com>
2018-08-31 11:52:11 +0300

    Create RNRon.js

---
+++ RNRon.js
@@ -0,0 +1,13 @@
+var namb = prompt('Номер РНР' );
+var place = prompt('Место проведения работ' );
+var time = prompt('Время пропазания услуг - Пример "7 часов" или "30 мин"' );
+var abon = prompt('Колличество стадающих абонентов' );
+
+
+
+
+var r = "РНР № " + namb + " на  оборудование связи, кабель, оборудование электропитания, информационные технологии МЕСТО ПРОВЕДЕНИЯ: " + place + " ВЛИЯНИЕ НА УСЛУГИ:  доступ в Интернет, интерактивное ТВ, Wi-Fi, VPN, телефонная связь, степень влияния на них: недоступность, ухудшение качества, кратковременное пропадание до " + time +  "  для " + abon + " клиентов.";
+
+var i = confirm(r);
+document.getElementById("group_interaction_info_form-tab_view-commentary").value = r;
+document.getElementById("group_interaction_info_form-tab_view-businessInteraction").value = "РНР №" + namb;
