9ad5248893c0b11ca0d4202e0b24201c23e5f9d3
nektoOi <32074045+nektoOi@users.noreply.github.com>
2019-02-19 13:38:25 +0300

    Update sms4.js

--- sms4.js
+++ sms4.js
@@ -27,7 +27,20 @@ var adres = $('#group_interaction_info_form-tab_view-node_rule_building_out-addr
 var timedown = $('#group_interaction_info_form-tab_view-calculate_due_date_form_out').text();
 var namberGP = $( "span[class='heading-accent']" ).text();
 
-var mess = "Недоступно оборудование " + adres +' '+ y + " Время начала аварии " + timedown+ " "+ namberGP ;
+var vse = new Array();
+var len = $(".ui-datatable-selectable").length;
+for(i=0;i<len;i++){
+var e = $(".ui-datatable-selectable").eq(i).find("td").eq(1).html();
+vse.push(e);
+}
+var sum = 0;
+for(var i=0;i<vse.length;i++){
+    sum = sum + parseInt(vse[i]);
+
+}
+alert(sum);
+
+var mess = "Недоступно оборудование " + adres +' '+ y + ". Количество установок" + sum +" Время начала аварии " + timedown+ " "+ namberGP ;
 //$("#group_interaction_info_form-tab_view-node_rule_building_out-node_rule_building_out").html('<input type="button" name="nubex" value="Nubex" />');
 
 
