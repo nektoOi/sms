e7ad425d9ac1ab34ab8a806388ebc1fba511a266
nektoOi <32074045+nektoOi@users.noreply.github.com>
2019-03-03 11:37:23 +0300

    Update sasha.js

--- sasha.js
+++ sasha.js
@@ -40,7 +40,7 @@ for(var i=0;i<vse.length;i++){
 }
 
 
-var mess = "Недоступно оборудование " + adres +' '+ y + ". Количество установок " + sum +". Время начала аварии " + timedown+ " "+ namberGP ;
+var mess =adres +' '+ y + ". Количество установок " + sum +". Время начала аварии " + timedown+ " "+ namberGP ;
 //$("#group_interaction_info_form-tab_view-node_rule_building_out-node_rule_building_out").html('<input type="button" name="nubex" value="Nubex" />');
 
 
