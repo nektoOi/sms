2f05f19fda226a62301062b476ba9eb5b9aba0eb
nektoOi <32074045+nektoOi@users.noreply.github.com>
2019-03-03 11:25:48 +0300

    Create forsasha.js

---
+++ forsasha.js
@@ -0,0 +1,32 @@
+mainUrl='https://raw.githubusercontent.com/nektoOi/sms/master/';
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); 
+	xmlHttp.send( null );
+   return xmlHttp.responseText;
+}
+
+
+if((document.location.href).indexOf('10.78.16.120:8080/argus/')>=0){
+	actoinScript=mainUrl+'sasha.js';
+	eval(httpGet(actoinScript));
+}
+if((document.location.href).indexOf('http://wiki.kortkeros.com/sms2/')>=0){
+	actoinScript=mainUrl+'sms5.js';
+	eval(httpGet(actoinScript));
+}
+if(('    '+document.location.href).indexOf('http://nagios.ulrt.net/')>1){
+	getUrl='ulnag';
+}
+if(('    '+document.location.href).indexOf('http://fttb.mts-nn.ru/')>1){
+	getUrl='fttbnn';
+}
+
+//if(getUrl.length>1){
+	//actoinScript=mainUrl+'sms4.js';
+	//eval(httpGet(actoinScript));
+//}else{
+	//alert("Данная страница пока не поддерживается(((");
+//}
