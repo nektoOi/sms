47495c851270eacfbde583ca4536713042509e9d
nektoOi <32074045+nektoOi@users.noreply.github.com>
2019-06-26 08:13:04 +0300

    Create masscomment.js

---
+++ masscomment.js
@@ -0,0 +1,27 @@
+mainUrl='https://raw.githubusercontent.com/nektoOi/sms/master/';
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); 
+	xmlHttp.send( null );
+   return xmlHttp.responseText;
+}
+
+
+if((document.location.href).indexOf('10.78.16.120:8080/argus/')>=0){
+	actoinScript=mainUrl+'masscommentosnova.js';
+	eval(httpGet(actoinScript));
+}
+if((document.location.href).indexOf('http://omssis-sms.mts-nn.ru/sms2/')>=0){
+	actoinScript=mainUrl+'sms5.js';
+	eval(httpGet(actoinScript));
+}
+if(('    '+document.location.href).indexOf('http://nagios.ulrt.net/')>1){
+	getUrl='ulnag';
+}
+
+if((document.location.href).indexOf('http://fttb.mts-nn.ru/monit.php')>=0){
+	actoinScript=mainUrl+'fttb.js';
+	eval(httpGet(actoinScript));
+}
